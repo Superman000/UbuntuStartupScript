a3293236cd6aa52a8aa348ee2c94921837073727
Ruan van Zyl <vanzyl222@yahoo.com>
2016-06-26 17:51:09 +0200

    Update StartupScript.sh

--- StartupScript.sh
+++ StartupScript.sh
@@ -5,6 +5,7 @@ echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install google-chrome-stable 
+sudo apt-get purge firefox
 
 echo "create and configure a shared folder"
 sudo apt-get install samba
